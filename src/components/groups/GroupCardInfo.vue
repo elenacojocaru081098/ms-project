da3638d27c9dc9d3bad37d02bf6a40a7ad3d63b4
Elena Cojocaru <elenacojocaru081098@gmail.com>
2023-09-07 14:40:34 +0300

    Fix groups display issue

--- src/components/groups/GroupCardInfo.vue
+++ src/components/groups/GroupCardInfo.vue
@@ -1,25 +1,29 @@
 <script setup lang="ts">
 import { storeToRefs } from 'pinia'
+import type { IGroup } from '@/interfaces/group'
 
-const { currentGroup } = storeToRefs(useGroupsStore())
+defineProps<{
+  group: IGroup
+}>()
+
 const { user } = storeToRefs(useUserStore())
 const { hasCoordinatorRights } = useUserPermission()
 </script>
 
 <template>
   <p class="text-body-2" v-if="hasCoordinatorRights(user)">
-    <span v-if="currentGroup.users">
+    <span v-if="group.users">
       {{
-        `${currentGroup.users.length} ${
-          currentGroup.users.length === 1 ? 'participant' : 'participanti'
+        `${group.users.length} ${
+          group.users.length === 1 ? 'participant' : 'participanti'
         }`
       }}
     </span>
   </p>
   <p class="text-body-2">
-    <span v-if="currentGroup.studies">
+    <span v-if="group.studies">
       {{
-        `${currentGroup.studies.length} ${currentGroup.studies.length === 1 ? 'studiu' : 'studii'}`
+        `${group.studies.length} ${group.studies.length === 1 ? 'studiu' : 'studii'}`
       }}
     </span>
   </p>
